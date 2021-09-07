6b8e554bdd0c1b37222ef350403e4fc9fa407c93
Carefree-Carmander <blow.carmen@gmail.com>
2021-09-07 14:37:09 -0400

    Delete src/critics directory

--- src/critics/critics.router.js
+++
@@ -1,7 +0,0 @@
-// const router = require("express").Router();
-// const controller = require("./critics.controller");
-// const methodNotAllowed = require("../errors/methodNotAllowed");
-
-// router.route("/").get(controller.list).all(methodNotAllowed);
-
-// module.exports = router;
--- src/critics/critics.server.js
+++
@@ -1,9 +0,0 @@
-// const knex = require("../db/connection");
-
-// function list() {
-//   return knex("critics").select("*");
-// }
-
-// module.exports = {
-//   list,
-// };
